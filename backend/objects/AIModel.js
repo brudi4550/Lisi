a57cbe8c9c04ac9255f0f7456480ae1ef1e62194
Alexander <alex.mayr.mobil@gmail.com>
2024-05-14 19:00:33 +0200

    updated prompt for first recommendation. Added refinement prompt

--- backend/objects/AIModel.js
+++ backend/objects/AIModel.js
@@ -20,21 +20,21 @@ export default class AIModel {
       presence_penalty: 1.15,
       frequency_penalty: 0.2,
       prompt:
-        "You are a banking consulting for a leasing bank. Your customer is " +
+        "Your customer is " +
         user.age +
         "years old, with an income of" +
         user.income +
-        " per month and a dailyCommute of " +
-        user.dailyCommute +
-        "km. The customer wants to lease a new car of the type" +
+        " per month.The customer wants to lease a new car of the type" +
         user.preferences +
         ", with a " +
         user.environment +
-        "environmental impact and the fuel type should be " +
+        "environmental awareness and the fuel type should be " +
         user.fuelType +
-        ". You now have to recommend some cars for the customer, according to the provided specifications.",
+        ". The car should be in the range of " +
+        user.priceRange +
+        "euros. You now have to recommend some cars for the customer, according to the provided specifications. Keep in mind to recommend the newest models with production dates near 2024",
       prompt_template:
-        "<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\nYou are a helpful assistant<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n{prompt}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n",
+        "<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\nYou are a banking consulting for a leasing bank and you have to recommend cars for the customer, according to the specifications.<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n{prompt}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n",
     };
     var answer = "";
     for await (const event of this.replicate.stream(
@@ -47,6 +47,48 @@ export default class AIModel {
     }
     console.log(
       "*************************************** \n LLM-export: \n" +
+        answer +
+        "\n***************************************"
+    );
+    return answer;
+  }
+
+  async refine(recommendations, userinput, user) {
+    if (userinput === null || recommendations === null) return; //TODO error handling
+    const input = {
+      top_k: 50,
+      top_p: 0.9,
+      max_tokens: 512,
+      min_tokens: 0,
+      temperature: 0.6,
+      presence_penalty: 1.15,
+      frequency_penalty: 0.2,
+      prompt:
+        "You already delivered the following recommendations: " +
+        recommendations +
+        "Now the customer " +
+        user +
+        " has the following refinements about the recommendations: " +
+        userinput +
+        "Refine your car recommendations and provide the modified ones to the customer. Again keep in mind to recommend the newest models with production dates near 2024.",
+      prompt_template:
+        "<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\nYou are a banking consulting for a leasing bank and you have to recommend cars for the customer, according to the specifications.<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n{prompt}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n",
+    };
+    var answer = "";
+    for await (const event of this.replicate.stream(
+      "meta/meta-llama-3-70b-instruct",
+      {
+        input,
+      }
+    )) {
+      answer += event.toString();
+    }
+    console.log(
+      "*************************************** \n LLM-export for " +
+        user.toString() +
+        " with the already recommended cars " +
+        recommendations +
+        " : \n" +
         answer +
         "\n***************************************"
     );
