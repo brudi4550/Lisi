b716990cee1e263a44b97a595f0d4f9937c2193a
Alexander <alex.mayr.mobil@gmail.com>
2024-05-14 18:59:24 +0200

    added cors

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,10 @@ import bodyParser from "body-parser";
 import express from "express";
 import indexRouter from "./router/indexRouter.js";
 import dotenv from "dotenv";
-
+import cors from "cors";
 const app = express();
+
+app.use(cors());
 
 app.use(bodyParser.json());
 
