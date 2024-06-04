371141fa8dc190398289fa63899bc105d3d26c2a
Alexander Mayr <alex.mayr.mobil@gmail.com>
2024-06-04 14:56:14 +0200

    implemented markdown formatting for text on car detail page

--- frontend/src/CarDetails.js
+++ frontend/src/CarDetails.js
@@ -1,54 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { getCarInformation } from "./api";
 import LoadingBar from "./LoadingBar";
+import ReactMarkdown from "react-markdown";
 
 function CarDetails({ car, handleBackButton }) {
   const [carInfo, setCarInfo] = useState("");
   const [showInfo, setShowInfo] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const handleGetInfo = async () => {
-    setLoading(true);
-    try {
-      const info = await getCarInformation(car);
-      setCarInfo(info);
-      setShowInfo(true);
-    } catch (error) {
-      console.error("Error getting car information:", error);
-    } finally {
-      setLoading(false);
-    }
-  };
+  useEffect(() => {
+    const fetchCarData = async () => {
+      setLoading(true);
+      try {
+        const info = await getCarInformation(car);
+        setCarInfo(info);
+        setShowInfo(true);
+      } catch (error) {
+        console.error("Error getting car information:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCarData();
+  }, [car]);
 
   return (
     <div className="app-content car-details">
+      {showInfo && (
+        <div className="button-row" style={{ textAlign: "left" }}>
+          <button className="back-button" onClick={handleBackButton}>
+            Back
+          </button>
+        </div>
+      )}
       <h2 className="selected-car-heading">Selected Car:</h2>
       <div className="car-details-container">
         <div className="car-name">
           <p>{car}</p>
         </div>
-        <div className="button-row">
-          <button className="back-button" onClick={handleBackButton}>
-            Back
-          </button>
-          {!showInfo && (
-            <button className="get-info-button" onClick={handleGetInfo}>
-              Get Information
-            </button>
-          )}
-        </div>
         {loading && <LoadingBar />}
         {showInfo && (
-          <div className="car-info-textbox" style={{ width: "100%" }}>
-            <textarea
-              value={carInfo}
-              onChange={() => {}}
-              rows={20}
-              style={{ width: "100%", boxSizing: "border-box" }}
-              placeholder="Car Information"
-              readOnly
-            />
+          <div
+            className="car-info-textbox"
+            style={{ width: "100%", textAlign: "left" }}
+          >
+            <ReactMarkdown>{carInfo}</ReactMarkdown>
           </div>
         )}
       </div>
